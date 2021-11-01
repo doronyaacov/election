662c38c14d251100e8a1399e2d5d51d7d9a46132
doronyaacov <82172901+doronyaacov@users.noreply.github.com>
2021-11-01 12:19:11 +0200

    Add files via upload

---
+++ election-UI/Controllers/ReportsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using election_DLL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace election_UI.Controllers
+{
+    [Route("reports")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        // GET: מחזירה את כל המפלגות
+        [HttpGet("parties")]
+        public IEnumerable<Party> Get()
+        {
+            return new dbManager().GetAllParties();
+        }
+
+        // GET מחזירה מצביעים מעיר
+        [HttpGet("getVoteByCity/{city}")]
+        public IActionResult GetCityVote(string city)
+        {
+            object votes = new dbManager().GetVotesByCity(city);
+            if(votes != null)
+            {
+                return Ok(votes);
+            }
+            return NotFound();
+        }
+
+        // GET מחזירה בוחרים שהצביעו למפלגה
+        [HttpGet("getVoteByParty/{party}")]
+        public IActionResult GetPartyVote(string party)
+        {
+            object votes = new dbManager().GetVotesByParty(party);
+            if (votes != null)
+            {
+                return Ok(votes);
+            }
+            return NotFound();
+        }
+
+
+        //// GET: api/<ReportsController>
+        //[HttpGet("voted")]
+        //public int Getvoted()
+        //{
+        //    return new dbManager().voted;
+        //}
+
+        //// GET api/<ReportsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+
+        //// POST api/<ReportsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ReportsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ReportsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ election-UI/Controllers/VotersController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using election_DLL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace election_UI.Controllers
+{
+    [Route("voters")]
+    [ApiController]
+    public class VotersController : ControllerBase
+    {
+        // GET: מחזירה את כל הרשאים לבחירה
+        [HttpGet]
+        public IEnumerable<Voter> Get()
+        {
+            return new dbManager().GetAllVoters();
+        }
+
+        // GET מחזירב בוחר לפי תעודת זהות ותאריך הנפקה
+        [HttpGet("{id}/{idDate}")]
+        public IActionResult Get(string id, string idDate)
+        {
+            Voter v = new dbManager().FindVoter(id, idDate);
+            if(v != null)
+            {
+                return Ok(v);
+            }
+            return NotFound();
+        }
+
+
+        // GET מחזירה בוחר לפי תעודת זהות
+        [HttpGet("getVoter/{id}")]
+        public IActionResult GetVoter(string id)
+        {
+            Voter v = new dbManager().GetVoter(id);
+            if (v != null)
+            {
+                return Ok(v);
+            }
+            return NotFound();
+        }
+
+
+
+        // GET מכניסה את הבחירה של הבוחר 
+        [HttpGet("voteConfirmed/{id}/{idDate}/{voteFor}")]
+        public IActionResult Get1(string id, string idDate, string voteFor)
+        {
+            Voter v = new dbManager().PlaceVote(id, idDate, voteFor);
+            if (v != null)
+            {
+                return Ok(v);
+            }
+            return NotFound();
+        }
+
+
+        //// POST api/<VotersController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<VotersController>/5
+        //[HttpPut]
+        //public void Put(string id)
+        //{
+            
+        //}
+
+        //// DELETE api/<VotersController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
